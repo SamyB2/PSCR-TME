65fc0dc29d2d54ca86e801c4a8fa1941de115db3
BELMELLAT sami <belmellatsamy@gmail.com>
2024-10-09 11:55:02 +0200

    add : TME4

--- TME4/src/Banque.cpp
+++ TME4/src/Banque.cpp
@@ -1,6 +1,7 @@
 #include "Banque.h"
 
 #include <iostream>
+#include <mutex>
 
 using namespace std;
 
@@ -9,9 +10,24 @@ namespace pr {
 void Banque::transfert(size_t deb, size_t cred, unsigned int val) {
 	Compte & debiteur = comptes[deb];
 	Compte & crediteur = comptes[cred];
+	recursive_mutex & d_mu = debiteur.getMutex();
+	recursive_mutex & c_mu = crediteur.getMutex();
+	if (deb == cred) {
+		return;
+	}
+	// utiliser les locks par priorité
+	if (deb < cred) {
+		d_mu.lock();
+		c_mu.lock();
+	} else {
+		c_mu.lock();
+		d_mu.lock();
+	}
 	if (debiteur.debiter(val)) {
 		crediteur.crediter(val);
 	}
+	c_mu.unlock();
+	d_mu.unlock();
 }
 size_t Banque::size() const {
 	return comptes.size();
@@ -20,11 +36,15 @@ bool Banque::comptabiliser (int attendu) const {
 	int bilan = 0;
 	int id = 0;
 	for (const auto & compte : comptes) {
+		compte.getMutex().lock();
 		if (compte.getSolde() < 0) {
 			cout << "Compte " << id << " en négatif : " << compte.getSolde() << endl;
 		}
 		bilan += compte.getSolde();
 		id++;
+	}
+	for (const auto & compte : comptes) {
+		compte.getMutex().unlock();
 	}
 	if (bilan != attendu) {
 		cout << "Bilan comptable faux : attendu " << attendu << " obtenu : " << bilan << endl;
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,17 +1,53 @@
 #include "Banque.h"
+#include <cstdlib>
+#include <thread>
+#include <unistd.h>
+#include <iostream>
 
 using namespace std;
+using namespace pr;
 
 const int NB_THREAD = 10;
+const int MONEY = 1000;
+const int NB_COMPTES = 5;
+
+void behave_q1(Banque & b) {
+	for (int i = 0; i < 10000; ++i) {
+		int c1 = rand() % b.size();
+		int c2 = rand() % b.size();
+		int value = 1 + (rand() % 99);
+		b.transfert(c1, c2, value);
+		sleep(0.2);
+	}
+}
+
+void comptable(Banque & b) {
+	b.comptabiliser(NB_COMPTES * MONEY);
+}
+
 int main () {
 	vector<thread> threads;
-	// TODO : creer des threads qui font ce qui est demandé
+	Banque b(NB_COMPTES, MONEY);
+	
+	// thread creation
+	threads.reserve(NB_THREAD);
 
+	// solde total
+	cout << "Solde total : " << b.size() * MONEY << endl;
+
+	for (int i = 0; i < NB_THREAD; ++i) {
+		threads.emplace_back(behave_q1, ref(b));
+	}
+
+	// thread comptable
+	threads.emplace_back(comptable, ref(b));
 
 	for (auto & t : threads) {
 		t.join();
 	}
 
-	// TODO : tester solde = NB_THREAD * JP
+	// bilan comptable
+	b.comptabiliser(NB_COMPTES * MONEY);
+
 	return 0;
 }
